cf281ac3b443d815fa7e8ea5c2ed04c8f0a0d63b
Rogerlv51 <949608370@qq.com>
2022-07-24 20:31:40 +0800

    数位五五

--- newcoder/BC151 数位五五.py
+++ newcoder/BC151 数位五五.py
@@ -4,3 +4,17 @@
 #输入一行包含两个整数a,b (1<= a<= b<=1000000)
 
 # 举例14和19的数位和为5和10，符合条件 
+
+## 注意区间里面的数不一定是2位数哦
+a, b = input().split(" ")
+a, b = eval(a), eval(b)
+count =0
+for item in range(a, b+1):
+    item = str(item)
+    total =0
+    for i in range(len(item)):
+        total+=int(item[i])
+    if total % 5 == 0:
+        count = count + 1
+
+print(count)
